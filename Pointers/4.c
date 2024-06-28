a930a04edb2fa17227cdc4faf1fbd369cf0b7b2b
Нурсат Монге-Байыр <nursatiy@gmail.com>
2024-06-28 23:38:25 +0700

    Pointers task 4

---
+++ Pointers/4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char* strstr(const char* str, const char* sub);
+
+int main() {
+  char str[30];
+  char sub[30];
+  scanf("%s", str);
+  scanf("%s", sub);
+  char* ptr = strstr(str, sub);
+  printf("%p\n", ptr);
+  return 0;
+}
+
+char* strstr(const char* str, const char* sub) {
+  if (*sub == '\0') {
+    return (char*)str;
+  }
+  for (; *str; ++str) {
+    if (*(str) == *sub) {
+      char* ptr = strstr(str + 1, sub + 1);
+      return (ptr) ? ptr - 1 : NULL;
+    }
+  }
+  return NULL;
+}
